303fdc9273894dada059770c02606bb4bac8704b
Ritchie Fu <fu00800340@loswego.k12.or.us>
2021-01-17 19:54:14 +0000

    added cogs file for queries (tcs, scss, wca)

---
+++ cogs/queries.py
@@ -0,0 +1,62 @@
+import discord
+from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
+class Queries(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def tcs(self, ctx):
+        links = []
+        query = "+".join(ctx.message.content.split()[2:])
+        if not query:
+            await ctx.send("Make sure to enter a search query.")
+            return
+
+        page = requests.get("https://www.thecubicle.com/search?type=product&q=" + query)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        try:
+            await ctx.send("https://www.thecubicle.com/" + soup.find('a', class_='product-grid-item')['href'])
+        except TypeError:
+            await ctx.send("No results found...")
+
+    @commands.command()
+    async def scss(self, ctx):
+        links = []
+        query = "+".join(ctx.message.content.split()[2:])
+        if not query:
+            await ctx.send("Make sure to enter a search query.")
+            return
+
+        page = requests.get("https://www.speedcubeshop.com/search?type=product&q=" + query)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        try:
+            await ctx.send("https://www.speedcubeshop.com/" + soup.find('a', class_='product-title')['href'])
+        except TypeError:
+            await ctx.send("No results found...")
+
+    @commands.command()
+    async def wcaid(self, ctx):
+        name = "+".join(ctx.message.content.split()[2:])
+        if not name:
+            await ctx.send("Be sure to enter a name to search up")
+        else:
+            url = ("https://www.worldcubeassociation.org/search?q=" + str(name)).replace(" ","%20")
+            page = requests.get(url)
+
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            try:
+                query = soup.find("table",{"class":"table table-nonfluid table-vertical-align-middle"}).a["href"]
+                await ctx.send("https://www.worldcubeassociation.org" + query)
+            except:
+                await ctx.send("No results found...")
+
+def setup(client):
+    client.add_cog(Queries(client))
+
--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import kociemba
 import keep_alive
 import os
 from dotenv import load_dotenv
-
 import random
 import re
-import requests
 import sqlite3
 import time
-import urllib.request
-from bs4 import BeautifulSoup
 from datetime import datetime
 from discord.ext import commands
-from pyTwistyScrambler import scrambler333, scrambler222, scrambler444, scrambler555, pyraminxScrambler,scrambler666,scrambler777,megaminxScrambler,squareOneScrambler,skewbScrambler,clockScrambler
+from pyTwistyScrambler import scrambler333, scrambler444, scrambler555
 from scrambles import *
 import asyncio
 import string
@@ -82,56 +78,6 @@ async def on_message(message):
         embed = discord.Embed(title = '', description = scrambleCommands[(messageSplit[0] + ' ' + messageSplit[1])](), color = 0x43a8ff)
         await channel.send(embed = embed)
 
-
-@bot.command()
-async def tcs(ctx):
-    links = []
-    query = "+".join(ctx.message.content.split()[2:])
-    if not query:
-      await ctx.send("Make sure to enter a search query.")
-      return
-
-    page = requests.get("https://www.thecubicle.com/search?type=product&q=" + query)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    
-    try:
-        await ctx.send("https://www.thecubicle.com/" + soup.find('a', class_='product-grid-item')['href'])
-    except TypeError:
-        await ctx.send("No results found...")
-
-@bot.command()
-async def scss(ctx):
-    links = []
-    query = "+".join(ctx.message.content.split()[2:])
-    if not query:
-      await ctx.send("Make sure to enter a search query.")
-      return
-
-    page = requests.get("https://www.speedcubeshop.com/search?type=product&q=" + query)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    
-    try:
-        await ctx.send("https://www.speedcubeshop.com/" + soup.find('a', class_='product-title')['href'])
-    except TypeError:
-        await ctx.send("No results found...")
-
-@bot.command()
-async def wcaid(ctx):
-    name = "+".join(ctx.message.content.split()[2:])
-    if not name:
-        await ctx.send("Be sure to enter a name to search up")
-    else:
-        url = ("https://www.worldcubeassociation.org/search?q=" + str(name)).replace(" ","%20")
-        page = requests.get(url)
-
-        soup = BeautifulSoup(page.content, 'html.parser')
-
-      
-        try:
-            query = soup.find("table",{"class":"table table-nonfluid table-vertical-align-middle"}).a["href"]
-            await ctx.send("https://www.worldcubeassociation.org" + query)
-        except:
-            await ctx.send("No results found...")
 
 @bot.command()
 async def users(ctx):
